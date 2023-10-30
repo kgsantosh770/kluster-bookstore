d672c713bd1abd05cd646049f30054b937ff749c
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-10-30 09:22:11 +0530

    filter functionality

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Book from '../../components/Book/Book'
-import { BOOK_LIST, maxQuery, startIndex, DEFAULT_IMAGE, authorQuery } from '../../Api';
+import { BOOK_LIST, maxQuery, startIndex, DEFAULT_IMAGE, authorQuery, GENRES, SINGLE_BOOK } from '../../Api';
 import { useParams } from 'react-router-dom';
 
 const Books = () => {
@@ -9,6 +9,9 @@ const Books = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [totalItems, setTotalItems] = useState(-1);
+    const genreRef = useRef(null);
+    const sortRef = useRef(null);
+
     useEffect(() => {
         async function getBooks() {
             const url = person ? `${BOOK_LIST}${authorQuery(person)}` : `${BOOK_LIST}${startIndex(page * 10)}${maxQuery(10)}`
@@ -33,6 +36,23 @@ const Books = () => {
         setLoading(!loading);
     }
 
+    async function applyFilter() {
+        setLoading(true);
+        let url = SINGLE_BOOK + '?q=';
+        if (genreRef.current.value !== 'Select genre')
+            url += 'genre:' + genreRef.current.value;
+        if (genreRef.current.value !== 'Select genre' && sortRef.current.value !== 'Select')
+            url += '&'
+        if (sortRef.current.value !== 'Select')
+            url += 'orderBy=' + sortRef.current.value
+        const response = await fetch(url);
+        const booksData = await response.json();
+        console.log(booksData)
+        setTotalItems(booksData.totalItems);
+        setBooks(booksData.items);
+        setLoading(false);
+    }
+
     return (
         <>
             {
@@ -40,6 +60,28 @@ const Books = () => {
                     'Loading ...' :
                     totalItems <= 0 ? <h1 style={{ margin: '3rem 0', textAlign: 'center' }}>No Results Found</h1> :
                         <>
+                            <div className='filter'>
+                                <p>Filters:</p>
+                                <div>
+                                    <label>Genre: </label>
+                                    <select style={{ textTransform: 'capitalize' }} ref={genreRef}>
+                                        <option key={0}>Select genre</option>
+                                        {
+                                            GENRES.map((genre, index) => <option key={index + 1} style={{ textTransform: 'capitalize' }}>{genre}</option>)
+                                        }
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Sort by: </label>
+                                    <select style={{ marginTop: '1rem', textTransform: 'capitalize' }} ref={sortRef}>
+                                        <option key={0}>Select</option>
+                                        {
+                                            ['relevance', 'newest'].map((genre, index) => <option key={index + 1} style={{ textTransform: 'capitalize' }}>{genre}</option>)
+                                        }
+                                    </select>
+                                </div>
+                                <button style={{ padding: '.25rem', marginTop: '1rem' }} onClick={() => applyFilter()}>Apply Filters</button>
+                            </div>
                             <p style={{ textAlign: 'center' }}>Page: {page + 1}</p>
                             <div className='books-row flex-wrap'>
                                 {
